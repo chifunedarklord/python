848fc4a7b762a73adf06539c861989d346bf7585
chúa tể bóng tối <soihoanghh2005@gmail.com>
2026-03-28 10:56:38 +0700

    Delete baitaptoantu directory

--- baitaptoantu/bai4.py
+++
@@ -1,5 +0,0 @@
-s1 = input()
-s2 = input()
-s3 = input()
-
-print(s1 + " " + s2 + " " + s3)
--- baitaptoantu/bai5.py
+++
@@ -1,8 +0,0 @@
-R = float(input())
-pi = 3.14
-
-CV = 2 * pi * R
-DT = pi * R * R
-
-print(CV)
-print(DT)
