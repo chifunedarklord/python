3e8e7ccc41d53845bffe41ec4a0e76f23822d5a5
chúa tể bóng tối <soihoanghh2005@gmail.com>
2026-04-01 18:02:29 +0700

    Delete Bùi Đức Đoàn/Chương 3/luyentapwhile/luyentapfor directory

--- Bùi Đức Đoàn/Chương 3/luyentapwhile/luyentapfor/bai1.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Nhập n: "))
-
-for i in range(1, n):
-    if 2 * i < n:
-        print(f"{2*i} = 2*{i}")
--- Bùi Đức Đoàn/Chương 3/luyentapwhile/luyentapfor/bai4.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Nhập n (<20): "))
-
-if n >= 20:
-    print("Nhập sai điều kiện!")
-else:
-    for i in range(1, n + 1):
-        if i % 5 == 0 or i % 7 == 0:
-            print(i, end=" ")
